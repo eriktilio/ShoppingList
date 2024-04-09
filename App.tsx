27c9bf4233d868526f1d45133a424f66cbca9133
erik.rey <erik.rey@itriad.org.br>
2024-04-09 17:38:23 -0400

    chore: changed icons for android

--- App.tsx
+++ App.tsx
@@ -50,7 +50,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e8e7e3',
+    backgroundColor: '#fff',
   },
   contentWrapper: {
     flex: 1,
